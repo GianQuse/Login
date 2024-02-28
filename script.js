035bdcbca3ad67c00fcde99571dbc0bf8c0e8309
GianQuse <107727901+GianQuse@users.noreply.github.com>
2024-02-28 22:33:34 +0000

    Copia

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const btnStuden = document.getElementById("student"),
+      btnTeacher = document.getElementById("teacher"),
       btnSignIn = document.getElementById("sign-in"),
       btnSignUp = document.getElementById("sign-up"),
-      btnMain = document.getElementById("main"),
+      btnMainTeacher = document.querySelector(".main-teacher"),
+      btnMainRegister = document.querySelector(".main-register"),
+      btMainLogin = document.querySelector(".main-login"),
       option = document.querySelector(".option"),
+      formTeacher = document.querySelector(".teacher"),
       formRegister = document.querySelector(".register"),
       formLogin = document.querySelector(".login");
 
-btnMain.addEventListener("click", e => {
-    formRegister.classList.add("hide");
+btnMainTeacher.addEventListener("click", e => {
+    formTeacher.classList.add("hide");
+    option.classList.remove("hide");
+})
+
+btMainLogin.addEventListener("click", e =>{
     formLogin.classList.add("hide");
+    option.classList.remove("hide");
+})
+
+btnMainRegister.addEventListener("click", e => {
+    formRegister.classList.add("hide");
     option.classList.remove("hide");
 })
 
@@ -20,6 +33,11 @@ btnSignIn.addEventListener("click", e => {
 btnSignUp.addEventListener("click", e => {
     formRegister.classList.remove("hide");
     formLogin.classList.add("hide");
+})
+
+btnTeacher.addEventListener("click", e => {
+    option.classList.add("hide");
+    formTeacher.classList.remove("hide");
 })
 
 btnStuden.addEventListener("click", e => {
