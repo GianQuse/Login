3d0ee5f9677368347667932f78c17768545cadde
GianQuse <107727901+GianQuse@users.noreply.github.com>
2024-02-28 14:06:08 +0000

    Final

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-const btnSignIn = document.getElementById("sign-in");
+const btnSignIn = document.getElementById("sign-in"),
+      btnSignUp = document.getElementById("sign-up"),
+      formRegister = document.querySelector(".register"),
+      formLogin = document.querySelector(".login");
+    
+btnSignIn.addEventListener("click", e => {
+    formRegister.classList.add("hide");
+    formLogin.classList.remove("hide");
+})
+
+btnSignUp.addEventListener("click", e => {
+    formRegister.classList.remove("hide");
+    formLogin.classList.add("hide");
+})
+
