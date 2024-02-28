7f31116dda802a12b53bf9f337d59dca67aeca64
GianQuse <107727901+GianQuse@users.noreply.github.com>
2024-02-28 18:21:34 +0000

    Pagina Inicio

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
-const btnSignIn = document.getElementById("sign-in"),
+const btnStuden = document.getElementById("student"),
+      btnSignIn = document.getElementById("sign-in"),
       btnSignUp = document.getElementById("sign-up"),
+      btnMain = document.getElementById("main"),
+      option = document.querySelector(".option"),
       formRegister = document.querySelector(".register"),
       formLogin = document.querySelector(".login");
-    
+
+btnMain.addEventListener("click", e => {
+    formRegister.classList.add("hide");
+    formLogin.classList.add("hide");
+    option.classList.remove("hide");
+})
+
 btnSignIn.addEventListener("click", e => {
     formRegister.classList.add("hide");
     formLogin.classList.remove("hide");
@@ -11,5 +20,10 @@ btnSignIn.addEventListener("click", e => {
 btnSignUp.addEventListener("click", e => {
     formRegister.classList.remove("hide");
     formLogin.classList.add("hide");
+})
+
+btnStuden.addEventListener("click", e => {
+    option.classList.add("hide");
+    formLogin.classList.remove("hide");
 })
 
